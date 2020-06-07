183be768ccf4d539b6cc82ab401ff1bfa239fc9f
King Chun O <kingchun1991@hotmail.com>
2020-06-07 10:28:25 +0800

    Revert "remove local image"
    This reverts commit a3c693751d663964c11ff59c32c28f69b240e803.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
 
-//const getImageSharp = (nodes, name) => nodes.find(contentNode => (contentNode.internal.type === 'File' && contentNode.name === name));
+const getImageSharp = (nodes, name) => nodes.find(contentNode => (contentNode.internal.type === 'File' && contentNode.name === name));
 
 exports.onCreateNode = ({
   node,
@@ -10,15 +10,15 @@ exports.onCreateNode = ({
   const { createParentChildLink } = actions;
 
   if (node.internal.owner === 'gatsby-source-google-spreadsheet') {
-    //const avatar = (node.avatar !== null) ? node.avatar : 'avatar';
+    const avatar = (node.avatar !== null) ? node.avatar : 'avatar';
     const nodes = getNodes();
-    // let imageSharpNode = getImageSharp(nodes, avatar);
-    // if (imageSharpNode === undefined) {
-    //   imageSharpNode = getImageSharp(nodes, 'avatar');
-    // }
+    let imageSharpNode = getImageSharp(nodes, avatar);
+    if (imageSharpNode === undefined) {
+      imageSharpNode = getImageSharp(nodes, 'avatar');
+    }
 
-    //createParentChildLink({ parent: node, child: imageSharpNode });
-    createParentChildLink({ parent: node });
+    createParentChildLink({ parent: node, child: imageSharpNode });
+
     return;
   }
 
